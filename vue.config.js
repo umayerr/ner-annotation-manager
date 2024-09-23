386e7d1d0cbce9470a5ec5ffdfff5da693304164
umayerr <60082710+umayerr@users.noreply.github.com>
2024-09-23 13:48:07 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,31 @@
-const webpack = require("webpack");
+// const webpack = require("webpack");
+
+// module.exports = {
+//   publicPath: "/ner-annotator/",
+//   outputDir: "./docs/",
+
+//   pluginOptions: {
+//     quasar: {
+//       importStrategy: "kebab",
+//       rtlSupport: false,
+//     },
+//   },
+
+//   configureWebpack: (config) => {
+//     return {
+//       plugins: [
+//         new webpack.DefinePlugin({
+//           APPLICATION_VERSION: JSON.stringify(
+//             require("./package.json").version
+//           ),
+//         }),
+//       ],
+//     };
+//   },
+
+//   transpileDependencies: ["quasar"],
+// };
 
 module.exports = {
-  publicPath: "/ner-annotator/",
-  outputDir: "./docs/",
-
-  pluginOptions: {
-    quasar: {
-      importStrategy: "kebab",
-      rtlSupport: false,
-    },
-  },
-
-  configureWebpack: (config) => {
-    return {
-      plugins: [
-        new webpack.DefinePlugin({
-          APPLICATION_VERSION: JSON.stringify(
-            require("./package.json").version
-          ),
-        }),
-      ],
-    };
-  },
-
-  transpileDependencies: ["quasar"],
+  publicPath: process.env.NODE_ENV === "production" ? "/ner-annotation-manager/" : "/",
 };
